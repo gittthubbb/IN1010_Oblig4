884666ba91879754bb16142d8f41b0144b0e0c8e
AdrianBjerketvedt <128593135+AdrianBjerketvedt@users.noreply.github.com>
2023-03-22 11:48:38 +0100

    Add files via upload

--- Legemiddel.java
+++ Legemiddel.java
@@ -1,27 +1,29 @@
-abstract class Legemiddel{
-    
-    public final String navn;
-    public int pris;
-    public final double virkestoff;
-    public final int ID;
-    private static int teller = 0;
+abstract class Legemiddel {
+
+    public final String navn;  // Legemidlets navn
+    private static int tellerId = 0;  // Gir nye resepter en unik ID. Oeker med for hver nye resept
+    public final int id;  // Dette legemidlets unike ID
+    int pris;  // Legemidlets pris
+    public final double virkestoff;  // Antall milligram med virkestoff legemidlet inneholder
 
 
-    protected Legemiddel(String navn, int pris, double virkestoff){
+    public Legemiddel(String navn, int pris, double virkestoff) {
         this.navn = navn;
         this.pris = pris;
         this.virkestoff = virkestoff;
-        ID = teller;
-        teller++;
-
+        id = tellerId++;  // Stiger med 1 for hvert nye legemiddel-objekt
     }
 
-    public int hentPris(){
+    public int hentPrisen()
+    {  // Returnerer produktets pris
         return pris;
     }
 
-    public void settNyPris(int nyPris){
+    public void settNyPris(int nyPris)
+    {  // Gir legemidlet en ny pris
         pris = nyPris;
     }
-}
 
+    abstract public String toString();  // Abstrakt metode for å override utskriften av tekst i konsollen
+
+}
